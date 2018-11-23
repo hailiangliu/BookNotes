2579514f912515f97b7aebfc2c965f15818e9006
liuhl <liuhl@yinhu.com>
2018-11-23 18:01:15 +0800

    添加代码

--- MyNetty/src/main/java/org/lig/mynetty/server/MyEchoServer.java
+++ MyNetty/src/main/java/org/lig/mynetty/server/MyEchoServer.java
@@ -3,6 +3,7 @@ package org.lig.mynetty.server;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -21,12 +22,12 @@ public class MyEchoServer {
     public void start() throws Exception{
         final MyEchoServerHandler echoServerHandler = new MyEchoServerHandler();
         // 1. 创建EventLoopGroup
-        EventLoopGroup group = new NioEventLoopGroup();
-
+        EventLoopGroup serverGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
         try{
             // 2. 创建serverBootstrap
             ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(group)
+            bootstrap.group(serverGroup, workerGroup)
                     .channel(NioServerSocketChannel.class) // 3. 指定所使用的NIO传输Channel
                     .localAddress(new InetSocketAddress(port))// 4. 使用指定的端口设置套接字地址
                     .childHandler(new ChannelInitializer<SocketChannel>() {//5. 添加一个EchoServerHandler到子Channel的ChannelPipeline
@@ -35,11 +36,13 @@ public class MyEchoServer {
                             ch.pipeline().addLast(echoServerHandler); //MyEchoServerHandler被标注为@Sharreable所以我们可以总是使用同样的实例
                         }
                     });
+            bootstrap.option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
 
             ChannelFuture f = bootstrap.bind().sync();//6. 异步的绑定服务器；调用sync()方法阻塞等待直到绑定完成
             f.channel().closeFuture().sync();         //7. 获取Channel的CloseFuture并阻塞当前线程直到它完成
         } finally {
-            group.shutdownGracefully().sync(); // 关闭EventLoopGroup，释放所有资源
+            serverGroup.shutdownGracefully().sync(); // 关闭EventLoopGroup，释放所有资源
         }
 
 
